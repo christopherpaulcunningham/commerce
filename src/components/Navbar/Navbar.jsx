3dae4da4cce2cb2312ddf249e60c47fe8637264b
christopherpaulcunningham <christopherpaulcunningham@users.noreply.github.com>
2021-03-20 14:49:05 +0100

    Hide the cart icon from the navbar when viewing the cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import logo from '../../assets/logo.png';
 import CartIcon from './CartIcon/CartIcon';
 
 const Navbar = ({ totalItems }) => {
+	const location = useLocation();
+
 	return (
 		<div className="navbar">
 			<Link to="/">
 				<img src={logo} className="logo" alt="logo" />
-			</Link>			
-			<CartIcon totalItems={totalItems} />
+			</Link>
+			{/* Hide the cart icon when viewing the cart */}
+			{location.pathname !== '/cart' && (
+				<CartIcon totalItems={totalItems} />
+			)}
 		</div>
 	);
 };
