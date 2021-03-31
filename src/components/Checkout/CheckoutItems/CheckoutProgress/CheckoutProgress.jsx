a7a8cf28f2e045ab9928c2b1a9c9226b6b158bc5
christopherpaulcunningham <christopherpaulcunningham@users.noreply.github.com>
2021-03-31 11:41:26 +0200

    Add icons to checkout progress bar

--- src/components/Checkout/CheckoutItems/CheckoutProgress/CheckoutProgress.jsx
+++ src/components/Checkout/CheckoutItems/CheckoutProgress/CheckoutProgress.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
 const CheckoutProgress = ({ checkoutStage, checkoutSteps }) => {
-    return (
-        <div className="checkout-progress">
-            {checkoutSteps.map((step, index) => {
-                return <div key={index} className={"progress-item" + (checkoutStage === index ? " active" : " inactive")}>{step}</div>
-            })}
-        </div>
-    )
-}
+	return (
+		<div className="checkout-progress">
+			{checkoutSteps.map((step, index) => {
+				return (
+					<div
+						key={index}
+						className={
+							'progress-item' +
+							(checkoutStage === index ? ' active' : ' inactive')
+						}
+					>
+						<img src={checkoutStage === index ? step.activeIcon : step.disabledIcon} className="checkout-progress-icon" />
+					</div>
+				);
+			})}
+		</div>
+	);
+};
 
-export default CheckoutProgress; 
+export default CheckoutProgress;
