b95f292608c016ed09b8dc6a6f40c0067bd1e6b2
christopherpaulcunningham <christopherpaulcunningham@users.noreply.github.com>
2021-04-07 12:33:07 +0200

    Add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+    console.log(currentYear);
+
+    return (
+        <div className="footer-container">
+            <div className="footer-section">
+                <span className="header">Visit Us</span>
+                <span className="bold-regular">Amunt Bike Co.</span>
+                <span>Calle Falsa 123,</span>
+                <span>Valencia, 46001</span>
+                <span>Spain</span>
+                <span className="address-email">info@amuntbikeco.es</span>
+                <span className="address-number">+34 123 456 789</span>
+            </div>
+            <div className="footer-section">
+                <span className="header">Products</span>
+                <Link to="/shop/bicycles" style={{ textDecoration: 'none', color: '#ffffff' }}><span className="product-link">Bicycles</span></Link>
+                <Link to="/shop/accessories" style={{ textDecoration: 'none', color: '#ffffff' }}><span className="product-link">Accessories</span></Link>
+            </div>
+            <div className="footer-section">
+                <span className="header">Social</span>
+                <a href="https://www.instagram.com" target="_blank" className="social-link">Instagram</a>
+                <a href="https://www.facebook.com" target="_blank" className="social-link">Facebook</a>
+                <a href="https://www.twitter.com" target="_blank" className="social-link">Twitter</a>
+            </div>
+            <div className="break"></div>
+            <div className="footer-section trademark">
+                © {currentYear} Christopher Cunningham                
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as ScrollToTop } from './ScrollToTop/ScrollToTop';
 export { default as Navbar } from './Navbar/Navbar';
+export { default as Footer } from './Footer/Footer';
 export { default as Homepage } from './Homepage/Homepage';
 export { default as ProductDetails } from './ProductDetails/ProductDetails';
 export { default as Cart } from './Cart/Cart';
