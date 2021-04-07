4de4ec29a0ba490838a922581b594157cef2c9e0
christopherpaulcunningham <christopherpaulcunningham@users.noreply.github.com>
2021-04-07 12:32:10 +0200

    Add a carousel for featured products and new products

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState} from 'react';
 
 import Product from '../Product/Product';
+import ImageCarousel from './ImageCarousel/ImageCarousel';
+import ProductCarousel from './ProductCarousel/ProductCarousel';
 
 const Homepage = ({ products }) => {
 
@@ -9,24 +11,19 @@ const Homepage = ({ products }) => {
 	useEffect(() => {
 		if(products.length){
 			let sortedProducts = products.sort((a,b) => (a.created < b.created) ? 1: -1);
-			setRecentlyAdded(sortedProducts.slice(0,3));
+			setRecentlyAdded(sortedProducts.slice(0,8));
 		}
 	}, [products])
 	
 	return (
 		<div className="homepage-container container">
+			<div className="splash-container">
+				<ImageCarousel />
+			</div>
 			{products ? <>
-				<span className="homepage-title">Featured Products</span>
-				{products.map((product, index) => 
-					<Product product={product} key={product.id} />
-				)}
-
 				<span className="homepage-title">Newest Products</span>
-				{recentlyAdded.length && recentlyAdded.map((product) => 
-					<Product product={product} key={product.id} />
-				)}
-			</> : "Loading..." }
-			
+				{recentlyAdded.length && <ProductCarousel products={recentlyAdded} />}
+			</> : "Loading..." }			
 		</div>
 	);
 };
---
+++ src/components/Homepage/ProductCarousel/ProductCarousel.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const ProductCarousel = ({ products }) => {
+    var settings = {
+		dots: false,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: false,
+                }
+              },
+              {
+                  breakpoint: 855,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                  }
+                },
+        ]
+	};
+
+    return (
+        <div className="product-carousel-container">
+            <Slider {...settings}>
+				{products && products.map((product) => (                    
+                    <div className="product-carousel-item-container" key={product.id}>
+                        <Link to={`/products/${product.id}`} style={{ textDecoration: 'none', color: '#000000' }} >
+                            <div className="product-carousel-item">
+                                <img
+                                    className="product-carousel-item-image"
+                                    src={product.media.source}
+                                    alt="Product"
+                                />
+                                <span className="product-carousel-item-name">{product.name}</span>
+                                <span className="product-carousel-item-price">{product.price.formatted_with_symbol}</span>
+                            </div>
+                        </Link>
+                    </div>
+				))}
+			</Slider>
+        </div>
+    )
+}
+
+export default ProductCarousel
