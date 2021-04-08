271eb0b0eb4d7b7592e20815cffc4fa807544d1f
christopherpaulcunningham <christopherpaulcunningham@users.noreply.github.com>
2021-04-08 14:04:55 +0200

    Show skeleton screen when shop products are loading

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
 import Product from '../Product/Product';
+import ProductSkeleton from '../shared/ProductSkeleton/ProductSkeleton';
 
 const Shop = ({ match, products }) => {
 	const history = useHistory();
@@ -18,19 +19,24 @@ const Shop = ({ match, products }) => {
 
 	const validateParams = () => {
 		let currentCategory = match.params.category;
-		if (currentCategory === 'bicycles' || currentCategory === 'accessories') {
-			setProductCategory(`${currentCategory[0].toUpperCase()}${currentCategory.slice(1)}`);
+		if (
+			currentCategory === 'bicycles' ||
+			currentCategory === 'accessories'
+		) {
+			setProductCategory(
+				`${currentCategory[0].toUpperCase()}${currentCategory.slice(1)}`
+			);
 
-            // Display the appropriate products - bikes, accessories, etc.
-            if (products.length > 1) {
-                let filteredList = products.filter((product) =>
-                    product.categories.some(
-                        (category) =>
-                            category.name.toLowerCase() === currentCategory
-                    )
-                );
-                setProductList(filteredList);
-            }
+			// Display the appropriate products - bikes, accessories, etc.
+			if (products.length > 1) {
+				let filteredList = products.filter((product) =>
+					product.categories.some(
+						(category) =>
+							category.name.toLowerCase() === currentCategory
+					)
+				);
+				setProductList(filteredList);
+			}
 		} else {
 			// Redirect to the home page.
 			history.push('/');
@@ -44,25 +50,18 @@ const Shop = ({ match, products }) => {
 					<span className="btn-home">Home</span>
 				</Link>
 				/<span className="product-header">{productCategory}</span>
-				{/* {!isLoading && product && <span className="product-header">{product.name}</span>} */}
 			</div>
 			<div className="shop-product-list">
-				{productList.length &&
-					productList.map((product) => (
-						<Product product={product} key={product.id} />
-					))}
-			</div>
-			{/* <div className="shop-flex-container">
-				<div className="shop-filter-options">
-					Filter by price
-				</div>
-				<div className="shop-product-list">
-					{productList.length &&
-						productList.map((product) => (
+				{productList.length
+					? productList.map((product) => (
 							<Product product={product} key={product.id} />
-						))}
-				</div>
-			</div> */}
+					  ))
+					: [...Array(12)].map((element, index) => (
+							<div className="shop-skeleton">
+								<ProductSkeleton key={index} />
+							</div>
+					  ))}
+			</div>
 		</div>
 	);
 };
